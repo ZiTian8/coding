d060c496186b94ca7f584617437184a1ef627929
zhh <zhenghh@shu.edu.cn>
2019-04-09 14:57:12 +0800

    TreeIsSymmetrical

---
+++ src/sword2offer/TreeIsSymmetrical.java
@@ -0,0 +1,39 @@
+/**
+ * @ClassName TreeIsSymmetrical
+ * @Description
+ * @Author HAHA
+ * @Version V1.0.0
+ * @Date 2019/4/9 14:23
+ */
+package sword2offer;
+
+public class TreeIsSymmetrical {
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    boolean isSymmetrical(TreeNode pRoot){
+        return isSymmetrical(pRoot,pRoot);
+    }
+    boolean isSymmetrical(TreeNode pRoot1,TreeNode pRoot2){
+        if(pRoot1==null&&pRoot2==null){
+            return true;
+        }
+        if(pRoot1==null||pRoot2==null){
+            return false;
+        }
+        if(pRoot1.val!=pRoot2.val){
+            return false;
+        }
+        return isSymmetrical(pRoot1.left,pRoot2.right)&&isSymmetrical(pRoot1.right,pRoot2.left);
+
+    }
+
+}
