fbee58c0d41def259a3754deab80cd3b109fc6ad
zhh <zhenghh@shu.edu.cn>
2019-04-09 14:58:56 +0800

    二叉树的镜像

--- src/sword2offer/TreeMirror.java
+++ src/sword2offer/TreeMirror.java
@@ -6,17 +6,18 @@
  * @Date 2019/4/9 14:12
  */
 package sword2offer;
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
 
-    public TreeNode(int val) {
-        this.val = val;
-
-    }
-}
 public class TreeMirror {
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
     public void Mirror(TreeNode root) {
         if(root==null){
             return;
