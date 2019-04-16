e3a33746bf537a35fee6862874692770b1940b79
zhh <zhenghh@shu.edu.cn>
2019-04-16 18:40:38 +0800

    更新之字型打印树

--- src/sword2offer/PrintTreeFromTopToBottom.java
+++ src/sword2offer/PrintTreeFromTopToBottom.java
@@ -9,6 +9,8 @@ package sword2offer;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 public class PrintTreeFromTopToBottom {
     private class TreeNode {
@@ -27,16 +29,16 @@ public class PrintTreeFromTopToBottom {
         ArrayList list = new ArrayList();
 
         LinkedList linkedList = new LinkedList();
-        if(root!=null){
+        if (root != null) {
             linkedList.add(root);
         }
-        while(!linkedList.isEmpty()) {
-            TreeNode tem=(TreeNode) linkedList.poll();//pool取出并删除
+        while (!linkedList.isEmpty()) {
+            TreeNode tem = (TreeNode) linkedList.poll();//pool取出并删除
             list.add(tem.val);
-            if(tem.left!=null){
+            if (tem.left != null) {
                 linkedList.offer(tem.left);//尾部添加
             }
-            if(tem.right!=null){
+            if (tem.right != null) {
                 linkedList.offer(tem.right);//尾部添加
             }
         }
@@ -44,37 +46,167 @@ public class PrintTreeFromTopToBottom {
 
 
     }
+
     //分层打印
-    public ArrayList<Integer> PrintFromTopToBottom2(TreeNode root) {
-        ArrayList list = new ArrayList();
-        ArrayList list2 = new ArrayList();
+    public ArrayList<ArrayList<Integer>> PrintFromTopToBottom2(TreeNode root) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList();
+        ArrayList<Integer> clist = new ArrayList<>();
 
         LinkedList linkedList = new LinkedList();//
-        if(root!=null){
+        if (root != null) {
             linkedList.add(root);
-            list2.add(1);
-        }
-        int num=0;//下一层有几个
-        int toBeprint=1;//当前层还有几个
-        while(!linkedList.isEmpty()) {
-            TreeNode tem=(TreeNode) linkedList.poll();
 
-            if(tem.left!=null){
+        }
+        int num = 0;//下一层有几个
+        int toBeprint = 1;//当前层还有几个
+        while (!linkedList.isEmpty()) {
+            TreeNode tem = (TreeNode) linkedList.poll();
+
+            if (tem.left != null) {
                 linkedList.offer(tem.left);
                 num++;
 
             }
-            if(tem.right!=null){
+            if (tem.right != null) {
                 linkedList.offer(tem.right);
                 num++;
             }
-            list.add(tem.val);
+            clist.add(tem.val);
             --toBeprint;
             //当前层没了
-            if(toBeprint==0){
-                list2.add(num);
-                toBeprint=num;//把下一层的个数复制给当前层
-                num=0;//下一层格式为0
+            if (toBeprint == 0) {
+                list.add(clist);
+                clist = new ArrayList<>();
+
+                toBeprint = num;//把下一层的个数复制给当前层
+                num = 0;//下一层格式为0
+            }
+        }
+        return list;
+
+
+    }
+    //之字型打印1
+    public ArrayList<ArrayList<Integer>> PrintFromTopToBottom3(TreeNode root) {
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList();
+        ArrayList<Integer> clist = new ArrayList<>();
+
+        LinkedList linkedList = new LinkedList();//
+        LinkedList linkedList2 = new LinkedList();//
+        if (root != null) {
+            linkedList.add(root);
+            linkedList2.add(root);
+        }
+
+        int cen = 2;
+        int num = 0;//下一层有几个
+        int toBeprint = 1;//当前层还有几个
+        while (!linkedList.isEmpty()) {
+            TreeNode tem = (TreeNode) linkedList.poll();
+            TreeNode tem1 = (TreeNode) linkedList2.poll();
+
+
+            if (cen % 2 == 0) {
+                if (tem.left != null) {
+                    linkedList.offer(tem.left);
+                    num++;
+
+                }
+                if (tem.right != null) {
+                    linkedList.offer(tem.right);
+
+                    num++;
+                }
+                if (tem1.right != null) {
+                    linkedList2.offer(tem1.right);
+                }
+                if (tem1.left != null) {
+                    linkedList2.offer(tem1.left);
+                }
+            } else {
+                if (tem.right != null) {
+                    linkedList.offer(tem.right);
+                    num++;
+                }
+                if (tem.left != null) {
+                    linkedList.offer(tem.left);
+
+                    num++;
+                }
+                if (tem1.left != null) {
+                    linkedList2.offer(tem1.left);
+                }
+                if (tem1.right != null) {
+                    linkedList2.offer(tem1.right);
+                }
+            }
+
+            clist.add(tem.val);
+            --toBeprint;
+            //当前层没了
+
+
+            if (toBeprint == 0) {
+                cen++;
+                list.add(clist);
+                clist = new ArrayList<>();
+                toBeprint = num;//把下一层的个数复制给当前层
+                num = 0;//下一层格式为0
+                LinkedList temp = new LinkedList();
+                temp = linkedList;
+                linkedList = linkedList2;
+                linkedList2 = temp;
+            }
+        }
+        return list;
+
+    }
+    //之字型打印2
+    public ArrayList<ArrayList<Integer>> PrintFromTopToBottom4(TreeNode root) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList();
+        ArrayList<Integer> clist = new ArrayList<>();
+
+        LinkedList linkedList = new LinkedList();//
+        int cen=0;
+
+        if (root != null) {
+            linkedList.add(root);
+
+        }
+        int num = 0;//下一层有几个
+        int toBeprint = 1;//当前层还有几个
+        while (!linkedList.isEmpty()) {
+            TreeNode tem = (TreeNode) linkedList.poll();
+
+            if (tem.left != null) {
+                linkedList.offer(tem.left);
+                num++;
+
+            }
+            if (tem.right != null) {
+                linkedList.offer(tem.right);
+                num++;
+            }
+            clist.add(tem.val);
+            --toBeprint;
+            //当前层没了
+            if (toBeprint == 0) {
+                cen++;
+                //当层数是2的倍数时，将clist反转一下
+                if(cen%2==0){
+                    ArrayList<Integer> temp=new ArrayList();
+                    for (int i = clist.size()-1; i >= 0; --i) {
+                        temp.add(clist.get(i));
+                    }
+                    clist=temp;
+
+                }
+                list.add(clist);
+                clist = new ArrayList<>();
+
+                toBeprint = num;//把下一层的个数复制给当前层
+                num = 0;//下一层格式为0
             }
         }
         return list;
