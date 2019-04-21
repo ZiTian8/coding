f81427ed641025a9c49d8b8bbdc316b7a6deb62d
zhh <zhenghh@shu.edu.cn>
2019-04-21 16:11:20 +0800

    cglib

---
+++ src/learn/cglib/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName HelloWorldImpl
+ * @Description
+ * @Author HAHA
+ * @Version V1.0.0
+ * @Date 2019/4/21 11:33
+ */
+package learn.cglib;
+
+public class HelloWorld{
+
+    public void sayHelloWord() {
+        System.out.println("HelloWord");
+    }
+}
